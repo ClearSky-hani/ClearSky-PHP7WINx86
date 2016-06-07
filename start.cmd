9d6c327c57d0a723bf642b118794e7118627992e
haniokasai♡ <htek@haniokasai.com>
2016-06-07 20:42:20 +0900

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE ClearSky server software for Minecraft: Pocket Edition
+TITLE ClearSky-php7 server software for Minecraft: Pocket Edition
 cd /d %~dp0
 
 if exist bin\php\php.exe (
@@ -9,7 +9,7 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist ClearSky.phar (
+if exist ClearSky-php7.phar (
 	set POCKETMINE_FILE=ClearSky-php7.phar
 ) else (
 	if exist src\pocketmine\PocketMine.php (
