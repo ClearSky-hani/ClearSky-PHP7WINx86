89bcd2476cd2363881b6b82037d502284d3a9239
haniokasai♡ <htek@haniokasai.com>
2016-06-07 20:41:38 +0900

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
 del ClearSky-php7.phar
-utils\bin\wget.exe https://github.com/ClearSky-hani/ClearSky-PHP7WINx86/raw/master/ClearSky-php7.phar
+utils\bin\wget.exe 　--no-check-certificate　https://github.com/ClearSky-hani/ClearSky-PHP7WINx86/raw/master/ClearSky-php7.phar
